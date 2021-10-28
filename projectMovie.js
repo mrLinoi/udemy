177eb2b1486a2c79c0bfa66349b700c1f0522c7d
Dmitry <mr.linoi@mail.ru>
2021-10-28 09:31:32 +0300

    add new func

--- projectMovie.js
+++ projectMovie.js
@@ -8,12 +8,30 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько вы его оцените?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько вы его оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++ ) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько вы его оцените?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;       
+        console.log('done');     
+    } else {
+        console.log('error');
+        i--;   
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Намана');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Очень много');
+} else {
+    console.log('Ошибка');
+}
+
 
 console.log(personalMovieDB);
